78974c21a250ce98d8974ec5f67dd3e69343df57
jgarbora <juan.garbora@gobio.com>
2022-07-26 11:34:01 -0500

    adding DB

--- src/main/java/com/black/monkey/my/election/api/MyElectionApi.java
+++ src/main/java/com/black/monkey/my/election/api/MyElectionApi.java
@@ -3,9 +3,6 @@ package com.black.monkey.my.election.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main entry point of application.
- */
 @SpringBootApplication
 public class MyElectionApi {
     public static void main(String[] args) {
