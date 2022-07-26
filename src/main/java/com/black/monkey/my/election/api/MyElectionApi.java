d9de6469a213b097849ed3076fb47deedeaa0a1e
jgarbora <juan.garbora@gobio.com>
2022-07-26 10:50:58 -0500

    kick off

---
+++ src/main/java/com/black/monkey/my/election/api/MyElectionApi.java
@@ -0,0 +1,14 @@
+package com.black.monkey.my.election.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point of application.
+ */
+@SpringBootApplication
+public class MyElectionApi {
+    public static void main(String[] args) {
+        SpringApplication.run(MyElectionApi.class, args);
+    }
+}
